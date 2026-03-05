dce310d703e1cd78043462b0c84734fdef98d950
Gustavo Starace <practicodecocina@gmail.com>
2026-03-05 20:32:04 +0100

    fix: agregado paso

--- practica6_2_Cifrado_Cesar/index.js
+++ practica6_2_Cifrado_Cesar/index.js
@@ -1,16 +1,19 @@
 let inputFrase = document.getElementById("codificar");
 let frase = document.getElementById("input-frase");
+let paso = document.getElementById("input-paso")
 let respuestaHTML = document.getElementById("frase-codificada")
-const clave = 3;
+
+
 function codificador() {
     let palabraCodificada = "";
     let fraseIngresada = frase.value.toUpperCase();
+    let desplazamiento = parseInt(paso.value)
     
     for (let i = 0; i < fraseIngresada.length; i++) {
         let codigo = fraseIngresada.charCodeAt(i);
 
         if (codigo >= 65 && codigo <= 90) {
-            let charCodificado = codigo + clave;
+            let charCodificado = codigo + desplazamiento;
             
             if (charCodificado > 90) {
                 charCodificado -= 26;
